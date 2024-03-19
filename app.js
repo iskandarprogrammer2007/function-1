02c3e2bf648b8a5f0556217bb293a981b398c852
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-03-19 20:53:23 +0500

    60-level

--- app.js
+++ app.js
@@ -8,7 +8,6 @@
 //     }
 // }
 
-
 // {
 //     function distanceBetweenPillars(numPillars, distanceBetween, width) {
 //       // Calculate total distance covered by pillars (including width)
@@ -20,4 +19,38 @@
 //       return distance;
 //     }
 //     console.log(distanceBetweenPillars(3,20,3));
+// }
+// {
+//   function positiveSum(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+
+//       if (arr[i] > 0) {
+//         sum += arr[i];
+//       }
+
+//     }
+//     return sum;
+//   }
+//   console.log(positiveSum([1,4,3]));
+// }
+// {
+//   function countPositivesSumNegatives(input) {
+//     if (!input || input.length === 0) {
+//       return [];
+//     }
+
+//     let countPositives = 0;
+//     let sumNegatives = 0;
+
+//     for (let i = 0; i < input.length; i++) {
+//       if (input[i] > 0) {
+//         countPositives++;
+//       } else if (input[i] < 0) {
+//         sumNegatives += input[i];
+//       }
+//     }
+
+//     return [countPositives, sumNegatives];
+//   }
 // }
